0f40c8660936ad1990b1830a48f81fd74fc993de
Emeka Chinedu <chinedufrderick@gmail.com>
2017-11-21 02:12:41 +0000

    little correction in the contact_mailer.rb file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    defaul to: 'esf.chinedu@gmail.com'
+    default to: 'esf.chinedu@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
